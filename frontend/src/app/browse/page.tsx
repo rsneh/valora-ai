d59e2ccf6ad551092ea0fe4bef771b1905be906e
Ron Sneh <ron@reflectinnovation.com>
2025-05-26 23:36:17 +0300

    refactor: Simplify BrowsePage component by removing unused search parameters and related logic

--- frontend/src/app/browse/page.tsx
+++ frontend/src/app/browse/page.tsx
@@ -3,29 +3,11 @@ import { ProductList } from "@/components/product/product-list"
 import { Product } from "@/types/product"
 import { getProducts } from "@/services/api/products"
 
-export default async function BrowsePage(props: {
-  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  const searchParams = await props.searchParams;
-  const {
-    // sort, 
-    q: searchValue,
-  } = searchParams as { [key: string]: string };
-  // const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
-
-  const products: Product[] = await getProducts(); // sortKey, reverse, query: searchValue
-  const resultsText = products.length > 1 ? 'results' : 'result';
+export default async function BrowsePage() {
+  const products: Product[] = await getProducts();
 
   return (
     <>
-      {searchValue ? (
-        <p className="mb-4">
-          {products.length === 0
-            ? 'There are no products that match '
-            : `Showing ${products.length} ${resultsText} for `}
-          <span className="font-bold">&quot;{searchValue}&quot;</span>
-        </p>
-      ) : null}
       {products.length > 0 ? (
         <div className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           <ProductList products={products} />
